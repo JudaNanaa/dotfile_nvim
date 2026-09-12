885a038641a35998f69ac8ceae87d9e9eb912e16
JudaNanaa <moussaadamou898@gmail.com>
2026-09-12 15:48:00 +0200

    add treesitter languages

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,21 +1,31 @@
 return {
-    "nvim-treesitter/nvim-treesitter",
-    branch = "main",
-    build = ":TSUpdate",
-    lazy = false,
-    config = function()
-        require("nvim-treesitter").setup({
-            install_dir = vim.fn.stdpath("data") .. "/site",
-        })
-        require("nvim-treesitter").install({
-            "rust", "python", "bash", "toml", "regex", "c", "cpp",
-        })
+	"nvim-treesitter/nvim-treesitter",
+	branch = "main",
+	build = ":TSUpdate",
+	lazy = false,
+	config = function()
+		require("nvim-treesitter").setup({
+			install_dir = vim.fn.stdpath("data") .. "/site",
+		})
 
-        vim.api.nvim_create_autocmd("FileType", {
-            pattern = { "rust", "python", "bash", "toml", "regex", "c", "cpp" },
-            callback = function()
-                vim.treesitter.start()
-            end,
-        })
-    end,
+		require("nvim-treesitter").install({
+			"rust",
+			"python",
+			"bash",
+			"toml",
+			"yaml",
+			"latex",
+			"html",
+			"regex",
+			"c",
+			"cpp",
+		})
+
+		vim.api.nvim_create_autocmd("FileType", {
+			pattern = { "rust", "python", "bash", "toml", "regex", "c", "cpp" },
+			callback = function()
+				vim.treesitter.start()
+			end,
+		})
+	end,
 }
